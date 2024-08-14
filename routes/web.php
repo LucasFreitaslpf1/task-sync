740830ac503f8851d0d815e8c9c6b142672fdc49
Hudson Rikelme <hudsonrikelme@hotmail.com>
2024-08-13 22:52:28 -0300

    Implementação da Autenticação

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
 
 Route::controller(UsuarioController::class)->group(
@@ -39,3 +39,19 @@ Route::controller(TarefaController::class)->group(
         Route::delete('/tarefa/{tarefa}','destroy')->name("tarefa.destroy");
     }
 );
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
